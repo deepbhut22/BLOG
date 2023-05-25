8572234a95e92eb1b065fb65bfdd49cb15b19334
deepbhut22 <110539168+deepbhut22@users.noreply.github.com>
2023-05-25 18:57:18 +0530

    Update app.js

--- api/app.js
+++ api/app.js
@@ -4,13 +4,26 @@ import mongoose from 'mongoose';
 import mongodb from 'mongodb';
 import cors from 'cors';
 import { log } from 'console';
+import exp from 'constants';
+import User from './models/user.js';
 
 const app = new express();
 app.use(cors());
+app.use(express.json());
+
+mongoose.connect("mongodb://127.0.0.1:27017/BLOG");
 
 
-app.post("/register" , (req,res)=>{
-    log(req.body.username)
+app.post("/register" , async(req,res)=>{
+    const{username , email , password} = req.body;
+    await User.create({
+        username,
+        email,
+        password
+    })
+    console.log(username);
+    console.log(email);
+    console.log(password);
 })
 
 app.get("/" , (req,res)=>{
