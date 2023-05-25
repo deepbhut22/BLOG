b56a4adbb0ddd6991755fed69ecbe664181f91fe
deepbhut22 <110539168+deepbhut22@users.noreply.github.com>
2023-05-25 18:55:23 +0530

    Update Register.jsx

--- client/blog-app/src/components/Register.jsx
+++ client/blog-app/src/components/Register.jsx
@@ -4,19 +4,20 @@ import { Link } from 'react-router-dom';
 const Register = () => {
 
     const[username , setusername] = useState("");
-    const[Password , setpassword] = useState("");
+    const[password , setpassword] = useState("");
     const[email , setemail] = useState("");
 
     async function register(event){
+        event.preventDefault();
         await fetch("http://localhost:3002/register" ,{
             method: 'POST',
-            body: JSON.stringify({username,email,Password}),
+            body: JSON.stringify({username,email,password}),
             headers: {'Content-Type' : 'application/json'},
         })
     }
     return (
         <div className='register'>
-            <from onSubmit={register}>
+            <form onSubmit={register}>
                 <input type='text' placeholder='Username'
                     value={username}
                     onChange={(event)=>{setusername(event.target.value)}}
@@ -28,14 +29,14 @@ const Register = () => {
                 />
                 <br/>
                 <input type='text' placeholder='Password' 
-                    value = {Password}
+                    value = {password}
                     onChange={(event)=>{setpassword(event.target.value)}}
                 />
                 <br/>
                 <button onSubmit={register} type='submit'>Register</button>
                 <p>Already Registered?</p>
                 <p>Click <Link to="/login">Here</Link> To Login</p>
-            </from>
+            </form>
             <button>press</button>
         </div>
     );
