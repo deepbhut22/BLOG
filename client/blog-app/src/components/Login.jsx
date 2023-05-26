e22ee9793b100e5468466416c17ab5aca13acd69
deepbhut22 <110539168+deepbhut22@users.noreply.github.com>
2023-05-26 17:59:08 +0530

    Update Login.jsx

--- client/blog-app/src/components/Login.jsx
+++ client/blog-app/src/components/Login.jsx
@@ -1,18 +1,53 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, Navigate } from 'react-router-dom';
 
 const Login = () => {
+
+    const[username , setusername] = useState("");
+    const[password , setpassword] = useState("");
+    const[isloggedin , setisloggedin] = useState(false);
+
+    const submitlogin = (event)=>{
+        event.preventDefault();
+        fetch("http://localhost:3002/login" , {
+            method: 'POST',
+            body: JSON.stringify({username , password}),
+            headers: {'Content-Type' : 'application/json'},
+            credentials: 'include'
+        }).then(res=>{
+            console.log(res);
+            if(res.ok === false){
+                console.log("firse dalo");
+                setusername("");setpassword("");
+                alert("Wrong credentials");
+            }
+            else{
+                setisloggedin(!isloggedin);
+            }
+        })
+    }
+
+    if(isloggedin){
+        return <Navigate to={'/'} />
+    }
+
     return (
-        <div className='login'>
-            <from>
-                <input type='text' placeholder='Username'/>
+        <div className='login' onSubmit={submitlogin}>
+            <form>
+                <input type='text' placeholder='Username'
+                    value={username}
+                    onChange={(event)=>{setusername(event.target.value)}}
+                />
                 <br/>
-                <input type='text' placeholder='Password' />
+                <input type='text' placeholder='Password' 
+                    value={password}
+                    onChange={(event)=>{setpassword(event.target.value)}}
+                />
                 <br/>
                 <button type='submit'>Login</button>
                 <p>Haven't Regestered yet?</p>
                 <p>Click <Link to="/register">Here</Link> To Register Your Self</p>
-            </from>
+            </form>
         </div>
     );
 };
